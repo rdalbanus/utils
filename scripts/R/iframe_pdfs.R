eefe56cfd62e4a89503cf45cf90f2340fe8f5ddc
Ricardo D'O. Albanus <dricardo@wustl.edu>
2024-06-19 17:00:28 -0500

    Fix embedded pdf plot heights

--- scripts/R/iframe_pdfs.R
+++ scripts/R/iframe_pdfs.R
@@ -3,6 +3,9 @@ library(knitr)
 knit_hooks$set(plot = function(x, options) {
   # Check if output format is pdf
   if (options$dev == "cairo_pdf") {
+    # Add the out.height, otherwise not included (default = 300 px)
+    options$out.height <- options$fig.height * 96
+    # Replace all the iframes with ember
     gsub("embed", "iframe", hook_plot_html(x, options))
   } else {
     # Default behavior for other output formats
