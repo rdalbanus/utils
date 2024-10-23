064de32067d39a2736f6dd3aaaf629b53753e505
Ricardo D'O. Albanus <dricardo@wustl.edu>
2024-10-23 11:30:16 -0500

    Add more flexible pdf frame recognition

--- scripts/R/iframe_pdfs.R
+++ scripts/R/iframe_pdfs.R
@@ -2,7 +2,7 @@ library(knitr)
 # library(tidyverse)
 knit_hooks$set(plot = function(x, options) {
   # Check if output format is pdf
-  if (options$dev == "cairo_pdf") {
+  if (grepl("pdf$", options$dev, ignore.case = TRUE)) {
     # Add the out.height, otherwise not included (default = 300 px)
     options$out.height <- options$fig.height * 96
     # Replace all the iframes with ember
