f7ba220ba9dcd763aaceed85cef9da2b9874fd50
Ricardo D'O. Albanus <dricardo@wustl.edu>
2025-03-05 14:02:58 -0600

    Add git repo creation to mkproj.sh

--- bin/mkproj.sh
+++ bin/mkproj.sh
@@ -24,5 +24,12 @@ mkdir -p ${controldir}/src/envs/scripts
 echo "#!/bin/bash" >>  ${controldir}/commands
 echo "cd ${workdir}" >> ${controldir}/commands
 
+git init ${controldir}
+echo "*.html" >> ${controldir}/.gitignore
+git -C ${controldir} add -A
+git -C ${controldir} commit -m "Initial commit"
+
 ln -s ${controldir}/* ${workdir}
 ln -s ${datadir} ${workdir}/data
+
+echo 'Sys.setenv(WORKDIR = "/home/dricardo/work")' > ${workdir}/.Rprofile
